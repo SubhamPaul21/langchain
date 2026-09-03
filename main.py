9ac15bbe668d469923611cc0ea99f72c5b6d8555
SubhamPaulExperis <subhampaul12@gmail.com>
2026-09-03 18:14:24 +0530

    added basic claude template code for testing

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from langchain!")
+import os
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+
+def test_claude():
+    claude_api_key = os.getenv("CLAUDE_API_KEY")
+    model = ChatAnthropic(
+        api_key=claude_api_key,
+        model_name="claude-haiku-4-5",
+        timeout=30,
+        stop=None,
+    )
+
+    messages = [
+        (
+            "system",
+            "You are a helpful translator. Translate the user sentence to French.",
+        ),
+        (
+            "human",
+            "I love programming.",
+        ),
+    ]
+
+    for chunk in model.stream(messages):
+        print(chunk.content, end="", flush=True)
 
 
 if __name__ == "__main__":
-    main()
+    test_claude()
